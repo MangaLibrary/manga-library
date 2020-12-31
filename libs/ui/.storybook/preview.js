2ad8235d92ffe9732dbda03dfcaf96f5147793a4
Piotr Stępniewski <190422+k3nsei@users.noreply.github.com>
2020-12-31 20:23:14 +0100

    chore: release 1.0.0
    Release-As: 0.0.0

---
+++ libs/ui/.storybook/preview.js
@@ -0,0 +1,51 @@
+/*!
+ * @license
+ * Copyright (c) 2021 Piotr Stępniewski <k3nsei.pl@gmail.com>
+ *
+ * Use of this source code is governed by an GPL-3.0 license that can be
+ * found in the LICENSE file in the root directory of this source tree.
+ */
+
+import { withKnobs } from '@storybook/addon-knobs';
+import { addons } from '@storybook/addons';
+import { addDecorator } from '@storybook/react';
+import React, { useEffect, useState } from 'react';
+
+import { MlUiThemeProvider, ThemeBuilder } from '../src/lib/theme';
+
+export const parameters = {
+  options: {
+    storySort: {
+      order: ['Theme'],
+    },
+  },
+};
+
+let isDarkModeEnabled = false;
+const channel = addons.getChannel();
+
+channel.on('globalsUpdated', (event) => {
+  const {
+    backgrounds: { value },
+  } = event.globals || {};
+
+  isDarkModeEnabled = value === '#333333';
+});
+
+addDecorator(withKnobs);
+addDecorator(withTheme);
+
+function withTheme(storyFn) {
+  return <StorybookThemeProvider>{storyFn()}</StorybookThemeProvider>;
+}
+
+function StorybookThemeProvider(props) {
+  const isDarkMode = isDarkModeEnabled;
+  const [theme, setTheme] = useState(new ThemeBuilder().build());
+
+  useEffect(() => {
+    isDarkMode ? setTheme(new ThemeBuilder().build()) : setTheme(new ThemeBuilder().build());
+  }, [isDarkMode, setTheme]);
+
+  return <MlUiThemeProvider theme={theme}>{props.children}</MlUiThemeProvider>;
+}
---
+++ libs/utils/src/lib/boolean/is-boolean.ts
@@ -0,0 +1,11 @@
+/*!
+ * @license
+ * Copyright (c) 2021 Piotr Stępniewski <k3nsei.pl@gmail.com>
+ *
+ * Use of this source code is governed by an GPL-3.0 license that can be
+ * found in the LICENSE file in the root directory of this source tree.
+ */
+
+export const isBoolean = (value: unknown): value is boolean => {
+  return typeof value === 'boolean';
+};
