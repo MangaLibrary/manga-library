d6ad92880aa0c2d0fdf237f82f292c7ae100dc00
Piotr Stępniewski <190422+k3nsei@users.noreply.github.com>
2020-12-31 20:23:14 +0100

    chore: release 0.0.0
    Release-As: 0.0.0

---
+++ libs/utils/src/lib/object/is-object.ts
@@ -0,0 +1,11 @@
+/*!
+ * @license
+ * Copyright (c) 2021 Piotr Stępniewski <k3nsei.pl@gmail.com>
+ *
+ * Use of this source code is governed by an GPL-3.0 license that can be
+ * found in the LICENSE file in the root directory of this source tree.
+ */
+
+export const isObject = (value: unknown): value is Record<any, any> => {
+  return Object.prototype.toString.call(value) === '[object Object]';
+};
